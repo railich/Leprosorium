3cf46ede7aa13071f1b7610a2e0c71cba0fc86f1
Rail Sabirov <railich@bk.ru>
2016-10-10 20:23:46 +0300

    Add page POST news and create table Posts

--- app.rb
+++ app.rb
@@ -2,11 +2,39 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
+
+configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_date DATE,
+    content TEXT
+  )'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
 get '/new' do
+  @message = ''
+  erb :new
+end
+
+post '/new' do
+  @content = params[:content]
+  @message = "You typed #{@content}"
+
   erb :new
 end
